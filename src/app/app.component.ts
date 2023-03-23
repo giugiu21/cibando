b6983b194b9cd76699002160b8b708a1ef54db5a
giugiu21 <giulia.alemanno.ga@gmail.com>
2023-03-23 13:10:07 +0100

    terza lezione mattina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,11 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 export class AppComponent {
   title = 'cibando';
 
-  images = [
-    {id: 1,
-      label: 'Spaghetti al pomodoro'}, 
-    {id: 2,
-      label: 'Tagliata di manzo'}, 
-    {id: 3,
-      label: 'Tiramisù'}
-  ];
-
-  percorso = "../assets/images/carousel-";
+  evidenziato = false;
 
 
+  onEvidenziazione(){
+    this.evidenziato = !this.evidenziato;
+  }
 }
 
