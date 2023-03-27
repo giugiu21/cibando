56c048570a36a6b322e7892b877e0b5531bd958b
giugiu21 <giulia.alemanno.ga@gmail.com>
2023-03-27 13:01:36 +0200

    aggiunta recipe-card component e modifiche varie a footer e navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { RecipesComponent } from './components/recipes/recipes.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { RecipeCardComponent } from './shared/recipe-card/recipe-card.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FooterComponent } from './shared/footer/footer.component';
     RecipesComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RecipeCardComponent
   ],
   imports: [
     BrowserModule,
